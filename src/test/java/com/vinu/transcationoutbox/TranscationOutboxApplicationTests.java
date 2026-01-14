a75ec19c1a99e360c3568224ede415de15e7b549
vinu0404 <22cd3034@rgipt.ac.in>
2026-01-14 18:48:29 +0530

    Initial commit

---
+++ src/test/java/com/vinu/transcationoutbox/TranscationOutboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinu.transcationoutbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscationOutboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
